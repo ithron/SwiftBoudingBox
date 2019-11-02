2212272b3ea0a1dec3d8d1fb9ffd14f83d57d578
Stefan Reinhold <stefan@sreinhold.com>
2019-11-02 21:52:03 +0100

    Remove Foundation imports

--- Sources/SwiftBoundingBox/SIMDExtensions.swift
+++ Sources/SwiftBoundingBox/SIMDExtensions.swift
@@ -5,15 +5,13 @@
 //  Created by Stefan Reinhold on 31.10.19.
 //
 
-import Foundation
-
 /// Extend floating point SIMD3 types to have a squared length and legth property
 extension SIMD3 where Scalar: FloatingPoint {
   /// The squared length of the vector, i.e. the squared 2-norm
   var squaredLength: Scalar { (self * self).sum() }
 
   /// The eucliedean length of the vector, i.e. the 2-norm
-  var length: Scalar { sqrt(squaredLength) }
+  var length: Scalar { squaredLength.squareRoot() }
 }
 
 /// Extend fixed width integer SIMD3 types to have a squared length property
--- Sources/SwiftBoundingBox/SwiftBoundingBox.swift
+++ Sources/SwiftBoundingBox/SwiftBoundingBox.swift
@@ -24,8 +24,6 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-import Foundation
-
 public typealias BoundingBoxf = BoundingBox<Float>
 public typealias BoundingBoxi = BoundingBox<Int64>
 
